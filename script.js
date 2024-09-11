279d14aa65625668a49329225cebd844f32fde5e
Riya Awal <reeyaawal123@gmail.com>
2024-09-11 18:37:14 +0545

    Added adjusting fontsize feature and responsiveness

--- script.js
+++ script.js
@@ -46,30 +46,6 @@ function changeTheme() {
           "quote":"Success seems to be connected with action. Successful people keep moving. They make mistakes but they don't quit.",
           "author":"Conrad Hilton"
        },
-     
-    
-    ],
-    "motivational":[
-       {
-          "quote":"The Best Way To Get Started Is To Quit Talking And Begin Doing.",
-          "author":"Walt Disney"
-       },
-       {
-          "quote":"The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
-          "author":"Winston Churchill"
-       },
-       {
-          "quote":"Don’t Let Yesterday Take Up Too Much Of Today.",
-          "author":"Will Rogers"
-       },
-       {
-          "quote":"You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
-          "author":"Unknown"
-       },
-       {
-          "quote":"It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.",
-          "author":"Vince Lombardi"
-       }
     ],
     "fun":[
        {
@@ -156,6 +132,11 @@ function ranQuote() {
   const quotes = quotesCategory[selectedCategory];
   index = Math.floor(Math.random() * quotes.length);
   showQuote();
+}
+function changeSizeBySlider() {
+   let slider = document.getElementById("slider");
+   quote.style.fontSize = slider.value + "px";
+   author.style.fontSize = slider.value + "px";
 }
   
 showQuote();
